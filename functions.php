c5805f3e9d702907894ff9b8acf08ef6a3365573
Robert Clarke <robert@brightred.digital>
2025-07-16 16:45:07 +0100

    Added link to developer resource

--- functions.php
+++ functions.php
@@ -62,3 +62,9 @@ function brightred_allow_svg_uploads( $mimes ) {
 }
 add_filter( 'upload_mimes', 'brightred_allow_svg_uploads' );
 
+// Link to developer resources in admin area
+
+function admin_footer_text() {
+    return '<a href="https://drive.google.com/drive/folders/1YFO52o1Rych3eJ9mKBTyDQKpo6smOD74?usp=sharing" target="_blank">Developer Reference</a>';
+}
+add_filter( 'admin_footer_text', 'admin_footer_text' );
