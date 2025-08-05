78a3f3b65aa76fbeba84a2bf60f454ffe9187f99
Robert Clarke <robert@brightred.digital>
2025-08-05 12:32:18 +0100

    Seperated functions into extra functions

---
+++ extra-fuctions.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+// Enable SVG uploads
+function brightred_allow_svg_uploads( $mimes ) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter( 'upload_mimes', 'brightred_allow_svg_uploads' );
+
+// Link to developer resources in admin area
+
+function admin_footer_text() {
+    return '<a href="https://drive.google.com/drive/folders/1YFO52o1Rych3eJ9mKBTyDQKpo6smOD74?usp=sharing" target="_blank">Developer Reference</a>';
+}
+add_filter( 'admin_footer_text', 'admin_footer_text' );
+
+
+
+// Add WooCommerce support if plugin is active
+function brightred_add_woocommerce_support() {
+    if ( class_exists( 'WooCommerce' ) ) {
+        add_theme_support( 'woocommerce' );
+    }
+}
+add_action( 'after_setup_theme', 'brightred_add_woocommerce_support' );
+
+// Add LearnDash support if plugin is active
+function brightred_add_learndash_support() {
+    if ( function_exists( 'learndash_init' ) ) {
+        add_theme_support( 'learndash' );
+    }
+}
+add_action( 'after_setup_theme', 'brightred_add_learndash_support' );
--- functions.php
+++ functions.php
@@ -21,10 +21,9 @@ function brightred_enqueue_styles() {
 }
 add_action('wp_enqueue_scripts', 'brightred_enqueue_styles');
 
-// Basic Info (requires ACF)
-
 $roots_includes = array(
 	'/functions/basic-info.php',
+	'/functions/extra-functions.php',
 );
 
 foreach($roots_includes as $file){
@@ -38,33 +37,4 @@ require_once $filepath;
 
 unset($file, $filepath); 
 
-// Add WooCommerce support if plugin is active
-function brightred_add_woocommerce_support() {
-    if ( class_exists( 'WooCommerce' ) ) {
-        add_theme_support( 'woocommerce' );
-    }
-}
-add_action( 'after_setup_theme', 'brightred_add_woocommerce_support' );
 
-// Add LearnDash support if plugin is active
-function brightred_add_learndash_support() {
-    if ( function_exists( 'learndash_init' ) ) {
-        add_theme_support( 'learndash' );
-    }
-}
-add_action( 'after_setup_theme', 'brightred_add_learndash_support' );
-
-
-// Enable SVG uploads
-function brightred_allow_svg_uploads( $mimes ) {
-    $mimes['svg'] = 'image/svg+xml';
-    return $mimes;
-}
-add_filter( 'upload_mimes', 'brightred_allow_svg_uploads' );
-
-// Link to developer resources in admin area
-
-function admin_footer_text() {
-    return '<a href="https://drive.google.com/drive/folders/1YFO52o1Rych3eJ9mKBTyDQKpo6smOD74?usp=sharing" target="_blank">Developer Reference</a>';
-}
-add_filter( 'admin_footer_text', 'admin_footer_text' );
