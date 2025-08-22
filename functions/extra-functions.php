513be4ab4aa5354aaa8380062bb5faec3f922347
Robert Clarke <robert@brightred.digital>
2025-08-22 14:05:45 +0100

    Update extra-functions.php

--- functions/extra-functions.php
+++ functions/extra-functions.php
@@ -13,19 +13,3 @@ function admin_footer_text() {
     return '<a href="https://drive.google.com/drive/folders/1YFO52o1Rych3eJ9mKBTyDQKpo6smOD74?usp=sharing" target="_blank">Developer Reference</a>';
 }
 add_filter( 'admin_footer_text', 'admin_footer_text' );
-
-// Add WooCommerce support if plugin is active
-function brightred_add_woocommerce_support() {
-    if ( class_exists( 'WooCommerce' ) ) {
-        add_theme_support( 'woocommerce' );
-    }
-}
-add_action( 'after_setup_theme', 'brightred_add_woocommerce_support' );
-
-// Add LearnDash support if plugin is active
-function brightred_add_learndash_support() {
-    if ( function_exists( 'learndash_init' ) ) {
-        add_theme_support( 'learndash' );
-    }
-}
-add_action( 'after_setup_theme', 'brightred_add_learndash_support' );
